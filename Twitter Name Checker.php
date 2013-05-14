5f3a608b7a3775b2b8dbd6692c006be67a53b5a9
Chris Breitinger <thewhitewox@gmail.com>
2013-05-14 08:59:13 -0500

    First Release

--- Twitter Name Checker.php
+++ Twitter Name Checker.php
@@ -1,16 +1,16 @@
 <?php
 set_time_limit(0);
 echo "_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n";
-echo "|          Mass Skype Checker by Furry~       |\n";
-echo "|   Usage: php skype.php InsertWordListHere   |\n";
+echo "|          Mass Twitter Checker by Furry~      |\n";
+echo "|   Usage: php twitter.php InsertWordListHere  |\n";
 echo "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n";
 if(isset($argv[1])){
     $file = file_get_contents($argv[1]);
     $trim = trim($file);
-    $sk = explode("\r\n", $trim);
+    $tw = explode("\r\n", $trim);
     $browser = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36';
-    foreach($sk as $sky){
-        $url = 'https://login.skype.com/json/validator?new_username='.$sky;
+    foreach($tw as $twi){
+        $url = 'https://twitter.com/users/username_available?username='.$twi;
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -20,12 +20,12 @@ if(isset($argv[1])){
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $result = curl_exec($ch);
         $json = json_decode($result, true);
-        if($json['status'] == 200){
-            echo "Skype ".$sky." is available!\n";
+        if($json['valid'] == 1){
+            echo "Twitter ".$twi." is available!\n";
             $fh = fopen('available.txt', 'a') or die("can't open file");
-            fwrite($fh, $sky."\n");
+            fwrite($fh, $twi."\n");
         } else {
-            echo "Skype ".$sky." is taken!\n";
+            echo "Twitter ".$twi." is taken!\n";
         }
     }
 }
