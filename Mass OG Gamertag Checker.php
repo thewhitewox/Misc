dbf672f20ff5603659f094e347efc60a7e39dfd0
Chris Breitinger <thewhitewox@gmail.com>
2013-05-14 15:46:24 -0500

    Updated it

--- Mass OG Gamertag Checker.php
+++ Mass OG Gamertag Checker.php
@@ -7,28 +7,25 @@ echo "| Usage: php gamertag.php InsertWordListHere  |\n";
 echo "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n";
 if(isset($argv[1])){
     $file = file_get_contents($argv[1]);
-    $trim = urlencode($file);
-    $gt = explode("%0D%0A", $trim);
-    $avail = array();
+    $trim = trim($file);
+    $gt = explode("\r\n", $trim);
     foreach($gt as $gamertag){
-  $url = 'http://www.xboxleaders.com/api/profile/'.urlencode($gamertag).'.xml';
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_HEADER, 0);
-  curl_setopt($ch, CURLOPT_TIMEOUT, 8);
-  $result = curl_exec($ch);
-  if(strpos($result, 'Invalid gamertag') === false){
-    echo "Gamertag ".$gamertag." is taken!\n";
-  } else {
-    echo "Gamertag ".$gamertag." is available!\n";
-    $avail[] .= $gamertag;
-  }
-    }
-    foreach($avail as $ava){
-    $file = 'Available.txt';
-    $handle = fopen($file, 'a');
-    fwrite($handle, $ava."\n");
+        $url = 'http://www.xboxleaders.com/api/1.0/profile.json?gamertag='.$gamertag;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 8);
+        $result = curl_exec($ch);
+        $json = json_decode($result, true);
+        print_r($json);
+        if($json['Stat'] !== 'fail'){
+            echo "Gamertag ".$gamertag." is taken!\n";
+            $fh = fopen('available.txt', 'a') or die("can't open file");
+            fwrite($fh, $pheed."\n");
+        } else {
+            echo "Gamertag ".$gamertag." is available!\n";
+        }
     }
 }
 ?>
